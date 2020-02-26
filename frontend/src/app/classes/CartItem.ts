da10228eaa462f94168ef7873830842499765cda
Alessandro Ricci <alessandro.ricci.1996@gmail.com>
2020-02-26 15:56:07 +0100

    fe: revisione funzione add to cart

--- frontend/src/app/classes/CartItem.ts
+++ frontend/src/app/classes/CartItem.ts
@@ -1,6 +1,6 @@
 import { Product } from './Product';
 
 export class CartItem {
-    public quantity: number;
+    public quantity: number ;
     public product: Product;
   }
--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -11,18 +11,32 @@ export class CartService {
 
   constructor() { }
 
+  isItemInCart = (item) : boolean => {
+
+
+    return this.items.find(i => i.product.id === item.product.id ) != null;
+   } 
+
   addItemsToCart = (quantity,product) => {
-    let ci:CartItem= {
+    const ci : CartItem= {
       quantity:quantity,
       product:product
     } 
-     this.items.push(ci);
- 
+    // se non c'è lo aggiunge
+    if(!this.isItemInCart(ci))
+      this.items.push(ci);
+      // find ritorna l'oggetto item
+    else  {
+      console.log("SONO ENTRATO CAZZO!");
+      this.items.find(i => i.product.id === product.id ).quantity += quantity;
+    }
   }
 
-  getTotalItemsCart = ()  =>{
-    
-    return this.items.length;
+  getTotalItemsCart = () =>{
+    let supp : number = 0;
+    for( let i of this.items)      
+      supp += i.quantity;
+    return supp ;
     
   }
   //fare un metodo che mi dice se c'è l'ho già nel carrello
