4472b958e76072aef5909a52dfe51fef4b220196
Cestari Stefano <CestariSt@valueteam.com>
2020-02-27 14:23:13 +0100

    FE: code clean

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
