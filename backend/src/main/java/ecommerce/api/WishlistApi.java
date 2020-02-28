ed4356a102f16bb50fb19d96cd1089e81dcfb588
Pelli Edoardo <PelliEd@valueteam.com>
2020-02-28 17:34:08 +0100

    bus su chek token

--- backend/src/main/java/ecommerce/api/WishlistApi.java
+++ backend/src/main/java/ecommerce/api/WishlistApi.java
@@ -12,44 +12,57 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
 import ecommerce.dao.WishlistDao;
 import ecommerce.dto.WishlistDto;
 
+@Path(value = "/wishlist")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@OpenAPIDefinition(info = @Info(title = "Web-API Service", version = "1.0", description = "Web-API Service APIs"))
+public class WishlistApi {
 
+	@EJB
+	WishlistDao wishlistdao;
 
-	@Path(value = "/wishlist")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public class WishlistApi {
-
-		@EJB
-		WishlistDao wishlistdao;
-
-		@GET
-		public List<WishlistDto> getAllWishlist() {
-			List<WishlistDto> listaWishlist = new ArrayList<>();
-			listaWishlist = wishlistdao.selectAll();
-			return listaWishlist;
-		}
-		
-		@GET
-		@Path("/getWishByID/{wishlistID}")
-		public List<WishlistDto> getWishlistBYUserID(@PathParam("wishlistID") int id) {
-			List<WishlistDto> listaWishlist = new ArrayList<>();
-			listaWishlist = wishlistdao.selectByWishID(id);
-			return listaWishlist;
-		}
-		
-		@POST
-		@Path("/regWish/{userID}/{productID}")
-		public void regUser( @PathParam("userID") int userID,  @PathParam("productID") int prodID) {
-			wishlistdao.insert(userID,prodID);
-			return;
-		}
-		
-		@DELETE
-		@Path("/deleteWishByID/{wishlistID}")
-		public void deleteWishlistByID(@PathParam("wishlistID") int id) {
-			wishlistdao.deleteWishlistByID(id);
-		}
+	@GET
+	@APIResponses(value = {
+			@APIResponse(responseCode = "200", description = "Get most recently added articles", content = @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.ARRAY, implementation = WishlistDto[].class))),
+			@APIResponse(responseCode = "500", description = "Internal service error") })
+	@Operation(summary = "Get most recently added articles", description = "Get most recently added articles")
+	public List<WishlistDto> getAllWishlist() {
+		List<WishlistDto> listaWishlist = new ArrayList<>();
+		listaWishlist = wishlistdao.selectAll();
+		return listaWishlist;
 	}
+
+	@GET
+	@Path("/getWishByID/{wishlistID}")
+	public List<WishlistDto> getWishlistBYUserID(@PathParam("wishlistID") int id) {
+		List<WishlistDto> listaWishlist = new ArrayList<>();
+		listaWishlist = wishlistdao.selectByWishID(id);
+		return listaWishlist;
+	}
+
+	@POST
+	@Path("/regWish/{userID}/{productID}")
+	public void regUser(@PathParam("userID") int userID, @PathParam("productID") int prodID) {
+		wishlistdao.insert(userID, prodID);
+		return;
+	}
+
+	@DELETE
+	@Path("/deleteWishByID/{wishlistID}")
+	public void deleteWishlistByID(@PathParam("wishlistID") int id) {
+		wishlistdao.deleteWishlistByID(id);
+	}
+}
