c5126286bf28da552ad44093a6ee551c78e4fdc4
LucaBertinotti94 <bertinottiluca94@gmail.com>
2020-03-02 11:32:15 +0100

    update

--- backend/src/main/java/ecommerce/api/WishlistApi.java
+++ backend/src/main/java/ecommerce/api/WishlistApi.java
@@ -1,11 +1,12 @@
 package ecommerce.api;
 
-import java.awt.PageAttributes.MediaType;
+//import java.awt.PageAttributes.MediaType; import errato
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
-import javax.net.ssl.SSLEngineResult.Status;
+//import javax.net.ssl.SSLEngineResult.Status; import errato
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,7 +14,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.xml.ws.Response;
+//import javax.xml.ws.Response; import errato
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import ecommerce.dao.WishlistDao;
 import ecommerce.dto.WishlistDto;
@@ -21,56 +24,57 @@ import ecommerce.exceptions.EcommerceException;
 
 
 
-	@Path(value = "/wishlist")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public class WishlistApi {
+  @Path(value = "/wishlist")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public class WishlistApi {
 
-		@EJB
-		WishlistDao wishlistdao;
+    @EJB
+    WishlistDao wishlistdao;
 
-		@GET
-		public Response getAllWishlist() throws EcommerceException {
-			List<WishlistDto> listaWishlist = new ArrayList<>();
-			listaWishlist = wishlistdao.selectAll();
-			if(listaWishlist==null || listaWishlist.size()==0) {
-				return Response.status(Status.NOT_FOUND).build();
-			}
-			return Response.ok(listaWishlist).build();
+    @GET
+    public Response getAllWishlist() throws EcommerceException {
+      List<WishlistDto> listaWishlist = new ArrayList<>();
+      listaWishlist = wishlistdao.selectAll();
+      if(listaWishlist==null || listaWishlist.size()==0) {
+        return Response.status(Status.NOT_FOUND).build();
+      }
+      return Response.ok(listaWishlist).build();
 
-		}
-		
-		@GET
-		@Path("/getWishByID/{wishlistID}")
-		public Response getWishlistBYUserID(@PathParam("wishlistID") int id) throws EcommerceException {
-			List<WishlistDto> listaWishlist = new ArrayList<>();
-			listaWishlist = wishlistdao.selectByWishID(id);
-			if(listaWishlist==null || listaWishlist.size()==0) {
-				return Response.status(Status.NOT_FOUND).build();
-			}
-			return Response.ok(listaWishlist).build();
+    }
+    
+    @GET
+    @Path("/getWishByID/{wishlistID}")
+    public Response getWishlistBYUserID(@PathParam("wishlistID") int id) throws EcommerceException {
+      List<WishlistDto> listaWishlist = new ArrayList<>();
+      listaWishlist = wishlistdao.selectByWishID(id);
+      if(listaWishlist==null || listaWishlist.size()==0) {
+        return Response.status(Status.NOT_FOUND).build();
+      }
+      return Response.ok(listaWishlist).build();
 
-		}
-		
-		@POST
-		@Path("/regWish/{userID}/{productID}")
-		public Response regUser( @PathParam("userID") int userID,  @PathParam("productID") int prodID) throws EcommerceException {
-			if(wishlistdao.insert(userID,prodID)>0) {
-				return Response.status(Status.NO_CONTENT).build();
-			}
-			return Response.status(Status.CONFLICT).build();
-		
-		}
-		
-		
-		@DELETE
-		@Path("/deleteWishByID/{wishlistID}")
-		public Response deleteWishlistByID(@PathParam("wishlistID") int id) throws EcommerceException {
-			if(wishlistdao.deleteWishlistByID(id)>0) {
-				return Response.status(Status.NO_CONTENT).build();
-			}
-			return Response.status(Status.CONFLICT).build();
-		
-		}
-		
-	}
+    }
+    
+    @POST
+    @Path("/regWish/{userID}/{productID}")
+    public Response regUser( @PathParam("userID") int userID,  @PathParam("productID") int prodID) throws EcommerceException {
+      if(wishlistdao.insert(userID,prodID)>0) {
+        return Response.status(Status.NO_CONTENT).build();
+      }
+      return Response.status(Status.CONFLICT).build();
+    
+    }
+    
+    
+    @DELETE
+    @Path("/deleteWishByID/{wishlistID}")
+    public Response deleteWishlistByID(@PathParam("wishlistID") int id) throws EcommerceException {
+      if(wishlistdao.deleteWishlistByID(id)>0) {
+        return Response.status(Status.NO_CONTENT).build();
+      }
+      return Response.status(Status.CONFLICT).build();
+    
+    }
+    
+  }
+  
