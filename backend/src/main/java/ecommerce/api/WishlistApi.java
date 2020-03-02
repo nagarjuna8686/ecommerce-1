112d0ae9a24c73127929cd8df33074e6b4a1945f
Pelli Edoardo <PelliEd@valueteam.com>
2020-03-02 14:44:09 +0100

    RISOLTE LIBRERIE

--- backend/src/main/java/ecommerce/api/WishlistApi.java
+++ backend/src/main/java/ecommerce/api/WishlistApi.java
@@ -1,11 +1,9 @@
 package ecommerce.api;
 
-import java.awt.PageAttributes.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
-import javax.net.ssl.SSLEngineResult.Status;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,7 +11,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.xml.ws.Response;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import ecommerce.dao.WishlistDao;
 import ecommerce.dto.WishlistDto;
